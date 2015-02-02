7c2f1716460e52300dfcbbf007320c4bfb96305b
Erwan Normand <normand.erwan@gmail.com>
2015-02-02 22:29:58 +0100

    Usage of minted package

--- pdfbibtex.sh
+++ pdfbibtex.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-pdflatex rapport.tex 
+pdflatex -shell-escape rapport.tex 
 
 bibtex rapport.aux 
 
-pdflatex rapport.tex 
-pdflatex rapport.tex 
+pdflatex -shell-escape rapport.tex 
+pdflatex -shell-escape rapport.tex 
 
 mv rapport.pdf "Rapport de stage TN09 (Erwan Normand - VideoStitch).pdf"
